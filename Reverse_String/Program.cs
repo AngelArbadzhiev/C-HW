ffa4167b31e04c847b3668ef6a91455da96c3b50
user <user@PENKA>
2023-06-25 07:56:06 +0300

    Add project files.

---
+++ Reverse_String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.Write("Enter a string that you wisho to be reverted: ");
+string userInput = Console.ReadLine();
+Console.WriteLine($"Here is the reverted string: {ReverseString(userInput)}");
+
+string ReverseString(string userInput)
+{
+    List<char> chars= new List<char>();
+    foreach (char ch in  userInput)
+    {
+        chars.Add(ch);
+    }
+    chars.Reverse();
+    string res = String.Join("", chars);
+    return res;
+}
